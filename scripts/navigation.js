9ce9da39751ca7beab30ebf9b5f39a4c345ad141
randymz10 <randymz10@gmail.com>
2025-07-12 02:04:52 -0300

    feat: Finished the top menu,  added the styles  and created some function with  js. Pending the courses section.

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+(() => {
+  const navButton = document.querySelector(".menu-navbar > button");
+
+  const navbar = document.querySelector(".navbar");
+
+  navButton.addEventListener("click", (e) => {
+    navbar.classList.toggle("active");
+  });
+})();
