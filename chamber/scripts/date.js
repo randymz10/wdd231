4bb69aa7d1c036cdf168c89835e8718635938237
randymz10 <randymz10@gmail.com>
2025-07-20 01:16:15 -0300

    Added the normalize.css and footer

---
+++ chamber/scripts/date.js
@@ -0,0 +1,7 @@
+(() => {
+  const currentYear = new Date();
+  const lastModified = `Last Modification: ${document.lastModified}`;
+
+  document.querySelector("#currentyear").innerHTML = currentYear.getFullYear();
+  document.querySelector("#lastModified").innerHTML = lastModified;
+})();
