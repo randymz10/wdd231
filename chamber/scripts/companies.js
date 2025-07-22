964fba418430bc2a18d8fa313dcd67aaee17966f
randymz10 <randymz10@gmail.com>
2025-07-22 08:56:40 -0300

    Corrected company names

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -1,41 +1,30 @@
-const url =
-  "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const url = "https://randymz10.github.io/wdd231/chamber/data/members.json";
 
 const cards = document.querySelector("#cards");
 
-const displayProphets = (prophets) => {
-  prophets.forEach((prophet) => {
-    const card = document.createElement("section");
-    const fullName = document.createElement("h2");
-    const portrait = document.createElement("img");
+const displayCompanies = (companies) => {
+  companies.forEach((company) => {
+    const card = document.createElement("div");
+    const logo = document.createElement("img");
+    const name = document.createElement("h2");
     const dateBirth = document.createElement("p");
     const placeBirth = document.createElement("p");
 
-    fullName.innerHTML = `${prophet.name} ${prophet.lastname}`;
-    dateBirth.innerHTML = `Date of Birth: ${prophet.birthdate}`;
-    placeBirth.innerHTML = `Place of Birth: ${prophet.birthplace}`;
-    portrait.src = prophet.imageurl;
-    portrait.alt = `Portrait of ${prophet.name} ${prophet.lastname}`;
-    portrait.loading = "lazy";
-    portrait.width = "340";
-    portrait.height = "440";
+    logo.src = `images/companies/${company.icon_filename}`
+    logo.loading = "lazy";
+    logo.width = "250";
+    logo.height = "250";
 
-    card.appendChild(fullName);
-    card.appendChild(dateBirth);
-    card.appendChild(placeBirth);
-    card.appendChild(portrait);
-
-    card.addEventListener('click', () => { card.classList.toggle});
-
+    card.appendChild(logo);
     cards.appendChild(card);
   });
 };
 
-const getProphetData = async () => {
+const getCompaniesData = async () => {
   const response = await fetch(url);
   const data = await response.json();
-  console.table(data.prophets);
-  displayProphets(data.prophets);
+  console.table(data);
+  displayCompanies(data);
 };
 
-getProphetData();
+getCompaniesData();
