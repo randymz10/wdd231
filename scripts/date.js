096649807a1c4060927bf0ea69570ef89b6d6b2b
randymz10 <randymz10@gmail.com>
2026-05-06 01:18:48 -0300

    Add initial website structure and functionality
    - Created index.html with SEO metadata, header, navigation, main content, and footer sections.
    - Implemented courses.js to manage course data and display functionality.
    - Added date.js for dynamic year and last modified date display.
    - Developed navigation.js for responsive navigation menu.
    - Introduced colors.css for color variables and styles.
    - Added larger.css and small.css for responsive design and layout.
    - Included normalize.css for consistent styling across browsers.

---
+++ scripts/date.js
@@ -0,0 +1,7 @@
+// Dates
+
+const currentYear = new Date();
+const lastModified = `Last Modification: ${document.lastModified}`;
+
+document.querySelector("#currentyear").innerHTML = currentYear.getFullYear();
+document.querySelector("#lastModified").innerHTML = lastModified;
---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+// Responsive navigation
+
+const navButton = document.querySelector("#hamburger-button");
+const navBar = document.querySelector("#nav-bar");
+
+navButton.addEventListener("click", () => {
+  navButton.classList.toggle("show");
+  navBar.classList.toggle("show");
+});
