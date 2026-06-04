a537551c3b3420e64e405aac9e5a1fd7aaf3703f
randymz10 <randymz10@gmail.com>
2026-06-04 15:39:48 -0300

    Add local companies display and user visit tracking
    - Implemented localCompanies.js to fetch and display company data from local JSON.
    - Created title.js to track user visits and display personalized messages based on last visit.
    - Added extra-larger.css for responsive styling of local companies section.

---
+++ chamber/scripts/localCompanies.js
@@ -0,0 +1,28 @@
+// const url = "https://randymz10.github.io/wdd231/chamber/data/companies.json";
+const localCompanies = document.querySelector("#local-companies");
+
+import localCompaniesData from "../data/companies.mjs";
+
+async function getCardsData() {
+  let localCompaniesString = "";
+
+  localCompaniesData.map((company) => {
+    localCompaniesString += /*html*/ `
+    <section class="companyCard">
+        <h2 class="companyCard-title">${company.name}</h2>
+        <figure class="companyCard-image">
+          <img src="images/local-companies/${company.icon_filename}" alt="${company.name}" width="300px" height="200px"/>
+        </figure>
+        <address class="companyCard-address">${company.address}</address>
+        <p class="companyCard-description">${company.description}</p>
+        <div class="companyCard-button">
+          <button class="button">learn more</button>
+        </div>
+    </section>
+    `;
+  });
+
+  localCompanies.innerHTML = localCompaniesString;
+}
+
+getCardsData();
