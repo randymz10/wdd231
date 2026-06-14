07834a11939c9c9e4c067cf6860fb8b63e069980
Randy Muñoz <randymz10@gmail.com>
2026-06-13 22:34:03 -0300

    Add partners data fetching script and update partner count

--- final/scripts/partners.js
+++ final/scripts/partners.js
@@ -1,13 +1,21 @@
 const BASE_URL = "https://fakelogo.com";
-const numOfPartners = 6;
+const numOfPartners = 4;
 
 const getPartnersData = async () => {
     try {
-        const response = await fetch(`${BASE_URL}/random?count=${numOfPartners}`);
+        const response = await fetch(`${BASE_URL}/api/random?count=${numOfPartners}`);
         const data = await response.json();
-        console.log(data)
-        
+        return data;
+
     } catch (error) {
         console.error(error)
     }
 };
+
+const createPartnerCard = (partner) => {
+    const partnertCard = document.createElement('div');
+    partnertCard.classList.add('card');
+    partnertCard.innerHTML = html
+ };
+
+getPartnersData();
