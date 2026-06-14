b89c6b15e545dc6a3041b2545e5e562f582976da
randymz10 <randymz10@gmail.com>
2026-06-14 01:07:19 -0300

    Add partners rendering functionality and styles for partner logos

--- final/scripts/partners.js
+++ final/scripts/partners.js
@@ -1,23 +1,39 @@
 const BASE_URL = "https://fakelogo.com";
 const numOfPartners = 4;
+const partnersElement = document.querySelector("#partners");
 
 const getPartnersData = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/api/random?count=${numOfPartners}`);
-        const data = await response.json();
-        return data;
-
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await fetch(
+      `${BASE_URL}/api/random?count=${numOfPartners}`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const createPartnerCard = (partner) => {
-    const partnertCard = document.createElement('div');
-    partnertCard.classList.add('card');
-    partnertCard.innerHTML = /*html*/ `
-        <h3>${partner.name}</h3>
-    `
- };
+  const partnertCard = document.createElement("div");
+  partnertCard.classList.add("card");
+  partnertCard.classList.add("partner");
+  partnertCard.innerHTML = /*html*/ `
+  <a href="${partner.company_url}" target="_blank">
+    <img class="partner-logo" src="${partner.image_url}" alt="${partner.name} logo" loading="lazy"/>
+  </a>      
+  
+        
+    `;
+  return partnertCard;
+};
 
-getPartnersData();
+const renderPartners = async () => {
+  const partners = await getPartnersData();
+  partners.map((partner) => {
+    const partnerCard = createPartnerCard(partner);
+    partnersElement.appendChild(partnerCard);
+  });
+};
+
+renderPartners();
